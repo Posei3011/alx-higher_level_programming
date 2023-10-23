523be05006987d381ea47bec78a5e48705b640e4
Posei3011 <posei3011.com>
2023-10-23 08:02:10 -0700

    Update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    """Print x elements of a list.
+    printed_elements = 0
 
-    Args:
-    my_list (list): The list to print elements from.
-    x (int): The number of elements of my_list to print
+    try:
+        for i in range(x):
+            print(my_list[i], end=' ')
+            printed_elements += 1
+    except IndexError:
+        pass
+    finally:
+        print()  # Print a new line to separate the output
 
-    Returns:
-    The number of elements printed.
-    """
-
-    ret = 0
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end="")
-            ret += 1
-        except IndexError:
-            break
-    print("")
-    Return
-
+    return printed_elements

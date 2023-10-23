66846d14ae3b26bc660a4a6ad7b1f1557a0c244d
Posei3011 <posei3011.com>
2023-10-23 08:27:27 -0700

    Updates

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
+        formatted_value = "{:d}".format(int(value))
+        print(formatted_value)
+        print()  # Print a new line to separate the output
         return True
     except (ValueError, TypeError):
         return False
-

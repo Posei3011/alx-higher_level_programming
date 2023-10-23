87e8c477505a97f5a4b714c4e3ee457b339bf837
Posei3011 <posei3011.com>
2023-10-23 09:00:32 -0700

    Updates

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
-    printed_integers = 0
-
-    try:
-        for item in my_list:
-            if printed_integers == x:
-                break  # Stop when 'x' integers have been printed
-
-            try:
-                formatted_item = "{:d}".format(int(item))
-                print(formatted_item, end=' ')
-                printed_integers += 1
-            except (ValueError, TypeError):
-                continue  # Skip non-integer values
-        print()  # Print a new line to separate the output
-    except IndexError:
-        pass  # Handle the case when 'x' is greater than the list length
-
-    return printed_integers
-
-if __name__ == "__main__":
+    i, c = 0, 0
+    while i < x:
+        try:
+            print("(:d)".format(my_list[i]), end='')
+            c += 1
+        except (ValueError, TypeError):
+            pass
+        i += 1
+        int()
+        return c

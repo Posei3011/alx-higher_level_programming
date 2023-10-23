d698a09ec3c6d173b58682363fff5cd01dda61ed
Posei3011 <posei3011.com>
2023-10-23 08:20:49 -0700

    Updates

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+def safe_print_list(my_list=None, x=0):
+    if my_list is None:
+        my_list = []
+
     printed_elements = 0
 
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print(my_list[i], end=' ')
             printed_elements += 1
-    except IndexError:
-        pass
-    finally:
-        print()  # Print a new line to separate the output
+        except IndexError:
+            break  # Exit the loop if an IndexError occurs
 
+    print()  # Print a new line to separate the output
     return printed_elements
